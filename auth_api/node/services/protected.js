33938e4fa8df8b9a15c2254792c3684fffb961a4
Derrick Ampire <ampire90@gmail.com>
2023-02-04 09:32:54 +0300

    authorization function added

--- auth_api/node/services/protected.js
+++ auth_api/node/services/protected.js
@@ -1,3 +1,24 @@
+// should I put this in a .env?
+const secretKey = 'my2w7wjd7yXF64FIADfJxNs1oupTGAuW';
+
 export const protectFunction = (authorization) => {
-  return 'test';
+  
+  const token = req.header('Authorization');
+
+  // check that token exists
+  if(!token) {
+    return res.status(403).send('Please add token');
+  }
+
+  try {
+    const verified = jwt.verify(token, secretKey);
+    
+    if(verified) {
+      res.send('Success!');
+    } else {
+      return res.status(401).send(error);
+    }
+  } catch (error) {
+    return res.status(401).send(error);
+  }
 }
