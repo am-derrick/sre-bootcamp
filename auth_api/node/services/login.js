271c8d2611b1f9ff66529e82c17af6b447d58bfc
Derrick Ampire <ampire90@gmail.com>
2023-02-13 17:58:47 +0300

    key user obj removed and expiration changed

--- auth_api/node/services/login.js
+++ auth_api/node/services/login.js
@@ -3,6 +3,8 @@ const crypto = require('crypto');
 const mysql = require('mysql'); 
 const util = require('util');
 
+import 'regenerator-runtime/runtime';
+
 // should I put this in a .env? It's not wise to share secret keys in a public repo
 const secretKey = 'my2w7wjd7yXF64FIADfJxNs1oupTGAuW'
 
@@ -10,7 +12,7 @@ export const loginFunction = async (username, password) => {
 
   // mysql database details
   const connection = mysql.createConnection({
-    host: 'sre-bootcamp-selection-challenge.cabf3yhjqvmq.us-east-1.rds.amazonaws.com/',
+    host: 'sre-bootcamp-selection-challenge.cabf3yhjqvmq.us-east-1.rds.amazonaws.com',
     user: 'secret',
     password: 'jOdznoyH6swQB9sTGdLUeeSrtejWkcw',
     database: 'bootcamp_tht',
@@ -32,7 +34,7 @@ export const loginFunction = async (username, password) => {
     const hashed = crypto.createHash('sha512').update(password + obj.salt).digest('hex');
 
     if(!hashed.localeCompare(obj.password)){
-      const token = jwt.sign({ user: obj.username }, { role: obj.role}, secretKey, { expiresIn: '24h' });
+      const token = jwt.sign({ role: obj.role}, secretKey, { expiresIn: '10m' });
       return token;
     }
     return null;
